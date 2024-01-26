5a8875d7e4da6753d3c0fa2cf300a07405019755
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-01-26 10:22:28 +0300

    add auth as reusable lib system-wide

--- apps/bims/src/app/app.routes.ts
+++ apps/bims/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{
+  path: 'auth',
+  async loadComponent() {
+    const m = await import('@bims/auth');
+    return m.AuthComponent;
+  },
+
+}];
--- apps/thoughts/src/app/app.routes.ts
+++ apps/thoughts/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{
+  path: 'auth',
+  async loadComponent() {
+    const m = await import('@bims/auth')
+    return m.AuthComponent
+  },
+}];
