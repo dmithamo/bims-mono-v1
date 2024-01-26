dead939c57a9da30a746506568f49a1e2476e6b8
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-01-26 09:32:15 +0300

    trim things a little

--- apps/thoughts/src/app/app.component.ts
+++ apps/thoughts/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { HeaderComponent } from '@bims/ui-kit';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'bims-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
