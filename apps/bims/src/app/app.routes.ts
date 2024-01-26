b13b40c319dc5d4322f8c9a3b8e3367a4e439ffd
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-01-26 10:30:49 +0300

    locate shared libs at root of libs

--- apps/bims/src/app/app.routes.ts
+++ apps/bims/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [{
   path: 'auth',
   async loadComponent() {
-    const m = await import('@bims/auth');
+    const m = await import('@bims/auth')
     return m.AuthComponent;
   },
 
