45703c0b40dda7634110c34f68b4b7ae1987d594
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-01-26 17:54:31 +0300

    wip tailwind: rm ui-kit

--- apps/portfolio/src/app/app.component.ts
+++ apps/portfolio/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeaderComponent } from '@bims/ui-kit';
 
 @Component({
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'bims-root',
   templateUrl: './app.component.html',
 })
