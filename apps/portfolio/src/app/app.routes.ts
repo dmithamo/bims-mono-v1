55c8d384a7319bbf22c6a702f33eec2b963e3799
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-02-14 13:48:13 +0300

    clean up orphaned code

--- apps/portfolio/src/app/app.routes.ts
+++ apps/portfolio/src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [
-  {
-    path: 'auth',
-    async loadComponent() {
-      const m = await import('@bims/auth');
-      return m.AuthComponent;
-    },
-  },
-];
+export const appRoutes: Route[] = [];
