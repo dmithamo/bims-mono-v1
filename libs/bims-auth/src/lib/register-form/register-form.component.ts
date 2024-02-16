e09aa0f8d9fa2969017cbf8502b0310d5210aa50
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-02-17 00:15:55 +0300

    build register form

--- libs/bims-auth/src/lib/register-form/register-form.component.ts
+++ libs/bims-auth/src/lib/register-form/register-form.component.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AppIcon, AppIconSize, ButtonComponent } from '@bims/bims-ui';
 
 @Component({
   selector: 'bims-register-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent, RouterLink],
   templateUrl: './register-form.component.html',
 })
-export class RegisterFormComponent {}
+export class RegisterFormComponent {
+  createAccount() {
+    console.log('Hello, world!');
+  }
+
+  submitBtnIconSize: AppIconSize = AppIconSize.large;
+  submitBtnIconName: AppIcon = AppIcon.send;
+}
