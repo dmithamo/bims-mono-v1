16cc281d15d221ebcaec785eb4ea3ab7f7df08df
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-02-16 16:58:12 +0300

    fix broken auth component

--- libs/bims-auth/src/lib/bims-auth/bims-auth.component.ts
+++ libs/bims-auth/src/lib/bims-auth/bims-auth.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'bims-bims-auth',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './bims-auth.component.html',
-  styleUrl: './bims-auth.component.css',
 })
 export class BimsAuthComponent {}
