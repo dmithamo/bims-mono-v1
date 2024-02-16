f8c98e9bb8d3883c82e14617924add22e2e2685e
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-02-16 18:51:19 +0300

    prepare for auth service!

--- libs/bims-auth/src/lib/auth-form/auth-form.component.ts
+++ libs/bims-auth/src/lib/auth-form/auth-form.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './auth-form.component.html',
 })
 export class AuthFormComponent {
-  @Input({ required: true }) appName: string = '';
   @Input({ required: true }) redirectUrl: string = '';
+
+  constructor() {}
 }
