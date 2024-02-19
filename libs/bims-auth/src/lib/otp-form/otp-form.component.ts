5cb36006f56014e39a5419d76a72b4cfc6588f1a
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-02-19 09:21:42 +0300

    enable copy+paste in otp field

--- libs/bims-auth/src/lib/otp-form/otp-form.component.ts
+++ libs/bims-auth/src/lib/otp-form/otp-form.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Renderer2, WritableSignal, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AppIcon, AppIconSize, ButtonComponent } from '@bims/bims-ui';
 
@@ -10,9 +10,11 @@ import { AppIcon, AppIconSize, ButtonComponent } from '@bims/bims-ui';
   templateUrl: './otp-form.component.html',
 })
 export class OtpFormComponent {
-  veriftOtp() {
-    console.log('Hello, world!');
-  }
+  constructor(private renderer: Renderer2) {}
+
+  showPasteDisabledMsg: WritableSignal<boolean> = signal(false);
+
+  verifyOtp() {}
 
   submitBtnIconSize: AppIconSize = AppIconSize.large;
   submitBtnIconName: AppIcon = AppIcon.send;
@@ -21,4 +23,50 @@ export class OtpFormComponent {
     this.username.slice(0, 4) +
     '***' +
     this.username.slice(this.username.length, -3);
+
+  OTP_LENGTH = 5;
+  getOtpFieldsArray() {
+    return Array(this.OTP_LENGTH);
+  }
+
+  handleKeydownInOtpField(e: KeyboardEvent, fieldIdx: number) {
+    const isDigit = /\d/.test(e.key);
+    if (
+      !isDigit &&
+      e.key !== 'Backspace' &&
+      e.key !== 'Delete' &&
+      e.key !== 'Tab' &&
+      !e.metaKey
+    ) {
+      e.preventDefault();
+      return;
+    }
+
+    if ((e.key === 'Backspace' || e.key === 'Delete') && +fieldIdx > 0) {
+      setTimeout(() => {
+        this.renderer.selectRootElement(`#otp-${+fieldIdx - 1}`).focus();
+      });
+    }
+
+    if (isDigit && +fieldIdx < this.OTP_LENGTH - 1) {
+      setTimeout(() => {
+        this.renderer.selectRootElement(`#otp-${+fieldIdx + 1}`).focus();
+      });
+    }
+  }
+
+  handleFocus(e: FocusEvent) {
+    (e.target as HTMLInputElement).select();
+  }
+
+  handlePaste(e: ClipboardEvent) {
+    const text = e.clipboardData?.getData('text') || '';
+    const digits = text.split('');
+
+    for (let i = 0; i < this.OTP_LENGTH; i++) {
+      this.renderer
+        .selectRootElement(`#otp-${i}`)
+        .setAttribute('value', digits[i]);
+    }
+  }
 }
