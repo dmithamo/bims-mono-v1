153b0a2dff3e0ed2eb686d80d7315a2610d78558
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-02-28 18:30:52 +0300

    make icons a little bigger

--- apps/portfolio/src/app/app.routes.ts
+++ apps/portfolio/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { NotFoundPageComponent } from '@bims/bims-ui';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '**',
+    component: NotFoundPageComponent,
+  },
+];
