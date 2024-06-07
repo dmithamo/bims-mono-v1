567b435d45d2f3d8d3fe3a41b51d5abc612ba5a0
dmithamo <dennis.mithamo@sportserve.co>
2024-06-07 11:11:07 +0300

    improve ui a bit

--- apps/portfolio/src/app/routes/homepage/homepage.component.ts
+++ apps/portfolio/src/app/routes/homepage/homepage.component.ts
@@ -2,6 +2,7 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PeriodCounterComponent, TempPageComponent } from '@bims/bims-ui';
 import { TBioData } from '../../utils/definitions';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'bims-homepage',
@@ -11,13 +12,14 @@ import { TBioData } from '../../utils/definitions';
     TempPageComponent,
     PeriodCounterComponent,
     NgOptimizedImage,
+    RouterLink,
   ],
   templateUrl: './homepage.component.html',
 })
 export class HomepageComponent {
   bioData: TBioData = {
     name: 'Dennis Mithamo',
-    tagline: 'Web developer',
+    tagline: 'Software developer',
     workedSinceDate: new Date('2018-11-05T05:00:00.000Z'),
     ghUsername: 'dmithamo',
     linkedInUsername: 'dennis-mithamo',
